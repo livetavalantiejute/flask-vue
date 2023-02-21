75bf876109248acd68b05f131f08459a6c4f293a
livetavalantiejute <l.valantiejute@gmail.com>
2023-02-21 18:33:29 +0200

    not working :(

--- server/app.py
+++ server/app.py
@@ -165,7 +165,7 @@ def test():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # port = int(os.environ.get("PORT", 5000))
+    app.run()
 
 
